45bb601ca01a844437c1b4a46fcc18c7f80db468
maticb <matic.bernik@marand.si>
2019-05-03 22:28:09 +0200

    rtvslo - regex

--- code/data_extraction.py
+++ code/data_extraction.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+from lxml import html #etree
+import re
 import json
 
 
@@ -43,22 +45,29 @@ def extract_data_rtvslo(document : str, method='xpath'):
              "Lead" : None,
              "Content" : None}
     #Extract required data (using chosen method) and fill-out article dictionary
-    if method == 'regex':
+    if method.lower() == 'regex':
         pass
-    elif method == 'xpath':
-        pass
-    
+    elif method.lower() == 'xpath':
+        tree = html.fromstring(document)
+        article["Title"] = tree.xpath('//div[@id="main-container"]/div[3]/div/header/h1')[0].text_content()
+        #article['Author'] = tree.xpath('//*[@id="main-container"]/div[3]/div/header/div[3]/div[1]/strong')[0].text_content()
+        article['Author'] = tree.xpath('//div[@class="author-timestamp"]/strong')[0].text_content()
+        article['PublishedTime'] = tree.xpath('//div[@class="author-timestamp"]')[0].text_content().split('|')[1].strip()
+        article['SubTitle'] = tree.xpath('//*[@id="main-container"]/div[3]/div/header/div[2]/text()')[0]
+        article['Lead'] = tree.xpath('//*[@id="main-container"]/div[3]/div/header/p/text()')[0]
+        article['Content'] = '\n\n'.join(tree.xpath('//*[@id="main-container"]/div[3]/div/div[2]/article/p/text()'))
+        
     data = json.dumps(article)
     return data
 
 
 for domain in PAGES_DIRS:
     html_list = load_htmls(domain) 
-    for html in html_list:
+    for html_name,html_raw in html_list.items():
         if 'overstock' in domain.name.lower():
-            print(extract_data_overstock(html))
+            print(extract_data_overstock(html_raw))
         elif 'rtvslo' in domain.name.lower():
-            print(extract_data_rtvslo(html))
+            print(extract_data_rtvslo(html_raw))
                 
 
 
