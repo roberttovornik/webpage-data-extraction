a3f3429b0a9a7af771e33ea22942c93f4303d0f2
maticb <matic.bernik@marand.si>
2019-05-04 00:50:45 +0200

    overstock - regex

--- code/data_extraction.py
+++ code/data_extraction.py
@@ -3,6 +3,7 @@ import os
 from lxml import html #etree
 import re
 import json
+import string
 
 
 PAGES_DIR = Path("../data/pages/")
@@ -32,7 +33,24 @@ def extract_data_overstock(document : str, method='xpath'):
     if method == 'regex':
         pass
     elif method == 'xpath':
-        pass
+        tree = html.fromstring(document)
+        listing_trees = tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor]')
+        delete_indices = [i for i in range(1,len(listing_trees)) if listing_trees[i].get('bgcolor') == listing_trees[i-1].get('bgcolor')]
+        for i in sorted(delete_indices, reverse=True):
+            del listing_trees[i]
+        listings = [{**listing_template} for l in listing_trees]
+        for i in range(len(listings)):
+            listings[i]['Title'] = listing_trees[i].xpath('./td[2]/a')[0].text_content()
+            listings[i]['ListPrice'] = listing_trees[i].xpath('./td[2]/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]')[0].text_content()
+            listings[i]['Price'] = listing_trees[i].xpath('./td[2]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]')[0].text_content()
+            saving = listing_trees[i].xpath('./td[2]/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]')[0].text_content()
+            saving = saving.split()
+            listings[i]['Saving'] = saving[0]
+            listings[i]['SavingPercent'] = saving[1][1:-1]
+            #listings[i]['Content'] = listing_trees[i].xpath('./td[2]/table/tbody/tr/td[2]/span/text()')[0]
+            listings[i]['Content'] = listing_trees[i].xpath('./td[2]/table/tbody/tr/td[2]')[0].text_content()
+            print(listings[i])
+            break
     
     data = json.dumps(listings)
     return data
@@ -65,9 +83,10 @@ for domain in PAGES_DIRS:
     html_list = load_htmls(domain) 
     for html_name,html_raw in html_list.items():
         if 'overstock' in domain.name.lower():
-            print(extract_data_overstock(html_raw))
-        elif 'rtvslo' in domain.name.lower():
-            print(extract_data_rtvslo(html_raw))
+            extract_data_overstock(html_raw)
+            #print(extract_data_overstock(html_raw))
+        #elif 'rtvslo' in domain.name.lower():
+        #    print(extract_data_rtvslo(html_raw))
                 
 
 
